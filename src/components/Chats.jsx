e97b9ca3a6b5ed197ad0f2549f16d86c874f010a
Ken <52821985+kenwoof4@users.noreply.github.com>
2022-09-10 18:15:28 -0500

    Revert "safe"
    This reverts commit d177eb35a4023e4479381f67903343eada0c2a49.

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -20,79 +20,10 @@ export const Chats = () => {
         />
         <div className="userChatInfo">
           <span>Jane</span>
-          <p>Helloo000000000000000000000000000000000</p>
-        </div>
-      </div>
-      <div className="userChat">
-        <img
-          src="https://images.pexels.com/photos/13399952/pexels-photo-13399952.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load"
-          alt=""
-        />
-        <div className="userChatInfo">
-          <span>Jane</span>
           <p>Hellooo</p>
         </div>
       </div>
-      <div className="userChat">
-        <img
-          src="https://images.pexels.com/photos/13399952/pexels-photo-13399952.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load"
-          alt=""
-        />
-        <div className="userChatInfo">
-          <span>Jane</span>
-          <p>Hellooo</p>
-        </div>
-      </div>
-      <div className="userChat">
-        <img
-          src="https://images.pexels.com/photos/13399952/pexels-photo-13399952.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load"
-          alt=""
-        />
-        <div className="userChatInfo">
-          <span>Jane</span>
-          <p>Hellooo</p>
-        </div>
-      </div>
-      <div className="userChat">
-        <img
-          src="https://images.pexels.com/photos/13399952/pexels-photo-13399952.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load"
-          alt=""
-        />
-        <div className="userChatInfo">
-          <span>Jane</span>
-          <p>Hellooo</p>
-        </div>
-      </div>
-      <div className="userChat">
-        <img
-          src="https://images.pexels.com/photos/13399952/pexels-photo-13399952.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load"
-          alt=""
-        />
-        <div className="userChatInfo">
-          <span>Jane</span>
-          <p>Hellooo</p>
-        </div>
-      </div>
-      <div className="userChat">
-        <img
-          src="https://images.pexels.com/photos/13399952/pexels-photo-13399952.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load"
-          alt=""
-        />
-        <div className="userChatInfo">
-          <span>Jane</span>
-          <p>Hellooo</p>
-        </div>
-      </div>
-      <div className="userChat">
-        <img
-          src="https://images.pexels.com/photos/13399952/pexels-photo-13399952.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load"
-          alt=""
-        />
-        <div className="userChatInfo">
-          <span>Jane</span>
-          <p>Hellooo</p>
-        </div>
-      </div>
+      
     </div>
   );
 };
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-export const Message = (props) => {
+export const Message = () => {
   return (
-    <div className={`message ${props.owner}`}>
+    <div className="message owner">
       <div className="messageInfo">
         <img src="https://images.pexels.com/photos/13399952/pexels-photo-13399952.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load" alt="" />
         <span>Just now</span>
@@ -12,6 +12,5 @@ export const Message = (props) => {
         <img src="https://images.pexels.com/photos/13399952/pexels-photo-13399952.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load" alt="" />
       </div>
     </div>
-    
   );
 };
