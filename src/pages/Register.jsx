f99183d53fc5ba47bdb4dc065e5906217c50e3c5
Ken <52821985+kenwoof4@users.noreply.github.com>
2022-09-12 01:31:49 -0500

    register missing info

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -12,47 +12,65 @@ import { doc, setDoc } from "firebase/firestore";
 
 export default function Register() {
   const [err, setErr] = useState(false);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     const displayName = e.target[0].value;
     const email = e.target[1].value;
     const password = e.target[2].value;
     const file = e.target[3].files[0];
-    try {
-      const res = await createUserWithEmailAndPassword(auth, email, password);
-      const storageRef = ref(storage, displayName);
+    if (displayName && email && password && file) {
+      try {
+        const res = await createUserWithEmailAndPassword(auth, email, password);
+        const storageRef = ref(storage, displayName);
 
-      const uploadTask = uploadBytesResumable(storageRef, file);
+        const uploadTask = uploadBytesResumable(storageRef, file);
 
-      // Register three observers:
-      // 1. 'state_changed' observer, called any time the state changes
-      // 2. Error observer, called on failure
-      // 3. Completion observer, called on successful completion
-      uploadTask.on(
-        (error) => {
-          setErr(true);
-        },
-        () => {
-          // Handle successful uploads on complete
-          // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
-            await updateProfile(res.user, {
-              displayName,
-              photoURL: downloadURL,
-            });
-            await setDoc(doc(db, "users", res.user.uid), {
-              uid: res.user.uid,
-              displayName,
-              email,
-              photoURL: downloadURL,
-            });
-          });
-        }
-      );
-    } catch (err) {
-      setErr(true);
+        // Register three observers:
+        // 1. 'state_changed' observer, called any time the state changes
+        // 2. Error observer, called on failure
+        // 3. Completion observer, called on successful completion
+        uploadTask.on(
+          (error) => {
+            setErr(true);
+          },
+          () => {
+            // Handle successful uploads on complete
+            // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+            getDownloadURL(uploadTask.snapshot.ref).then(
+              async (downloadURL) => {
+                await updateProfile(res.user, {
+                  displayName,
+                  photoURL: downloadURL,
+                });
+                await setDoc(doc(db, "users", res.user.uid), {
+                  uid: res.user.uid,
+                  displayName,
+                  email,
+                  photoURL: downloadURL,
+                });
+              }
+            );
+          }
+        );
+      } catch (err) {
+        setErr(true);
+      }
+    } else {
+      const errMessage = document.getElementById("err");
+      if (!displayName) {
+        errMessage.textContent = "Missing display name!";
+      } else if (!email) {
+        errMessage.textContent = "Missing email!";
+      } else if (!password) {
+        errMessage.textContent = "Missing password!";
+      } else if (!file) {
+        errMessage.textContent = "Missing file!";
+      }
     }
   };
+
+  const handleImageChange = () => {};
 
   return (
     <div className="formContainer">
@@ -63,14 +81,20 @@ export default function Register() {
           <input type="text" placeholder="display name" />
           <input type="email" placeholder="email" />
           <input type="password" placeholder="password" />
-          <input style={{ display: "none" }} type="file" id="file" />
+          <input
+            onChange={handleImageChange}
+            style={{ display: "none" }}
+            type="file"
+            id="file"
+          />
           <label htmlFor="file">
             <img src={Add} alt="" />
             <span>Add an avatar</span>
           </label>
-
+          <div className="preview"></div>
           <button>Sign up</button>
           {err && <span>Something went wrong</span>}
+          <span id="err"></span>
         </form>
         <p>You have an account? Login</p>
       </div>
